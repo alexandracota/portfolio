b1889d86034a7a050ef34040f2cac40c1c9cf90a
Alex Cota <cota.alexandra14@gmail.com>
2017-08-12 19:41:23 -0700

    node modules and typed.js

---
+++ logic.js
@@ -0,0 +1,10 @@
+$(document).on('load', function() {
+
+	// import Typed from 'typed.js';
+
+	var typed = new Typed('.lead', {
+	  strings: ["Full Stack", "Web Developer"],
+	  typeSpeed: 30
+	});
+
+});
